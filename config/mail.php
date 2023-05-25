34da1ab5a9fec071e6724b057264266e6df6b110
Jayan Ratna <jayanratna@gmail.com>
2023-05-25 18:02:01 +1200

    configure Resend

--- config/mail.php
+++ config/mail.php
@@ -46,6 +46,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
